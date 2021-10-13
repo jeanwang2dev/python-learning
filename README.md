1fd4ea5b15bf607b0e33727dff33c4c3cd85a8dd
jeanwang2dev <jeanharmsen@yahoo.com>
2021-10-13 17:44:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# python-learning
+## Python for Beginners - Learn Python in 1 Hour
+[Youtube Link](https://www.youtube.com/watch?v=kqtD5dpn9C8)
+
---
+++ app.py
@@ -0,0 +1,11 @@
+# patient_name = input("what is the patient name?")
+# age = 2020 - int( input("what is the patient's birth year?") )
+# is_new = False
+# end_str = 'and he is a new patient' if is_new else 'is not a new patient '
+# result_str = patient_name + ' is ' + str(age) + ' years old ' + end_str
+# print(result_str)
+
+num_one = float(input("First: "))
+num_two = float(input("Second: "))
+the_sum = num_one + num_two
+print('Sum: ' + str(the_sum))
